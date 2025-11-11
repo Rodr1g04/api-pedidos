802ead4d85d4e1eec9954b4c06f34f5ac828d57d
Rodrigo Araújo <rodrigolav@hotmail.com>
2025-11-10 23:41:26 -0300

    Implementadas boas práticas REST e versionamento da API (Issue #5)

--- api-pedidos/app.js
+++ api-pedidos/app.js
@@ -1,22 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
-const connectDB = require('./config/database');
-const authRouter = require('./routes/authRouter');
-const pedidosRouter = require('./routes/pedidosRouter');
+const connectDB = require('./v1/config/database'); 
 
 connectDB();
 
 const app = express();
+
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/auth', authRouter);
-app.use('/api/pedidos', pedidosRouter);
+const pedidosRouter = require('./v1/routes/pedidosRouter');
+const authRouter = require('./v1/routes/authRouter'); 
+
+
+app.use('/api/v1/pedidos', pedidosRouter);
+app.use('/api/v1/auth', authRouter);
 
 module.exports = app;
